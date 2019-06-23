8139256418022e51820b769558c8363cfab16dc5
duo.xi <xiduo>
2019-06-23 16:57:06 +0800

    支持jsp页面

--- src/main/java/com/example/webdemo/WebdemoApplication.java
+++ src/main/java/com/example/webdemo/WebdemoApplication.java
@@ -2,9 +2,16 @@ package com.example.webdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebdemoApplication {
+public class WebdemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebdemoApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebdemoApplication.class, args);
